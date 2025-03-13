008bda4346b4ec369925511667107c05578940ce
LaraGomezN <n.lara@uniandes.edu.co>
2025-03-13 10:15:52 -0500

    cambiosssss

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import '../styles/login.css';
 import Card from 'react-bootstrap/Card';
+import HomeCenter from './HomeCenter';
 
 function Home() {
     return (
         <div className="div-home">
+            <HomeCenter />
         </div>
     );
 }   
--- src/components/HomeCenter.js
+++ src/components/HomeCenter.js
@@ -1,7 +1,28 @@
 import Card from 'react-bootstrap/Card';
-
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 
 export default function HomeCenter() {
-    
-
+    return (
+        <Card className="card-home">
+            <Card.Body>
+                <Card.Title className="card-title">Welcome to the Home Page</Card.Title>
+                <Row>
+                    <Col>
+                        <Image src="../imgs/menu.png" fluid />
+                        <p>Texto debajo de la imagen de menú</p>
+                    </Col>
+                    <Col>
+                        <Image src="../imgs/tienda.png" fluid />
+                        <p>Texto debajo de la imagen de tienda</p>
+                    </Col>
+                    <Col>
+                        <Image src="../imgs/compra.png" fluid />
+                        <p>Texto debajo de la imagen de compra</p>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Card>
+    )
 }
