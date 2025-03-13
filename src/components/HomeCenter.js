4f6d36968f8df2e48a7633779d993be1330632c7
LaraGomezN <n.lara@uniandes.edu.co>
2025-03-13 10:15:58 -0500

    cambiossss

--- src/components/HomeCenter.js
+++ src/components/HomeCenter.js
@@ -2,24 +2,24 @@ import Card from 'react-bootstrap/Card';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+import '../styles/login.css'; // Importa el archivo CSS
 
 export default function HomeCenter() {
     return (
         <Card className="card-home">
             <Card.Body>
-                <Card.Title className="card-title">Welcome to the Home Page</Card.Title>
                 <Row>
                     <Col>
-                        <Image src="../imgs/menu.png" fluid />
-                        <p>Texto debajo de la imagen de menú</p>
+                        <Image src="../imgs/menu.png" fluid className="small-image" />
+                        <h1>MENU</h1>
                     </Col>
                     <Col>
-                        <Image src="../imgs/tienda.png" fluid />
-                        <p>Texto debajo de la imagen de tienda</p>
+                        <Image src="../imgs/tienda.png" fluid className="small-image" />
+                        <h1>STORES</h1>
                     </Col>
                     <Col>
-                        <Image src="../imgs/compra.png" fluid />
-                        <p>Texto debajo de la imagen de compra</p>
+                        <Image src="../imgs/compra.png" fluid className="small-image" />
+                        <h1>CART</h1>
                     </Col>
                 </Row>
             </Card.Body>
